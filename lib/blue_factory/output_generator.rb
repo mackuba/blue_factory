fcc8510cb4deeb9cdc1f3c72c93f8d9fa28dd04a
Kuba Suder <jakub.suder@gmail.com>
2025-10-08 01:48:34 +0200

    oops, forgot to wrap the class in a module

--- lib/blue_factory/output_generator.rb
+++ lib/blue_factory/output_generator.rb
@@ -1,81 +1,83 @@
 require_relative 'errors'
 
-class OutputGenerator
-  AT_URI_REGEXP = %r(^at://did:plc:[a-z0-9]+/app\.bsky\.feed\.post/[a-z0-9]+$)
+module BlueFactory
+  class OutputGenerator
+    AT_URI_REGEXP = %r(^at://did:plc:[a-z0-9]+/app\.bsky\.feed\.post/[a-z0-9]+$)
 
-  def generate(response)
-    output = {}
+    def generate(response)
+      output = {}
 
-    raise InvalidResponseError, ":posts key is missing" unless response.has_key?(:posts)
-    raise InvalidResponseError, ":posts should be an array" unless response[:posts].is_a?(Array)
+      raise InvalidResponseError, ":posts key is missing" unless response.has_key?(:posts)
+      raise InvalidResponseError, ":posts should be an array" unless response[:posts].is_a?(Array)
 
-    output[:feed] = response[:posts].map { |x| process_post_element(x) }
+      output[:feed] = response[:posts].map { |x| process_post_element(x) }
 
-    if cursor = response[:cursor]
-      output[:cursor] = cursor.to_s
+      if cursor = response[:cursor]
+        output[:cursor] = cursor.to_s
+      end
+
+      if req_id = response[:req_id]
+        output[:reqId] = req_id.to_s
+      end
+
+      output
     end
 
-    if req_id = response[:req_id]
-      output[:reqId] = req_id.to_s
+    def process_post_element(object)
+      if object.is_a?(String)
+        validate_uri(object)
+        { post: object }
+      elsif object.is_a?(Hash)
+        process_post_hash(object)
+      else
+        raise InvalidResponseError, "Invalid post entry, expected string or hash: #{object.inspect}"
+      end
     end
 
-    output
-  end
+    def process_post_hash(object)
+      post = {}
 
-  def process_post_element(object)
-    if object.is_a?(String)
-      validate_uri(object)
-      { post: object }
-    elsif object.is_a?(Hash)
-      process_post_hash(object)
-    else
-      raise InvalidResponseError, "Invalid post entry, expected string or hash: #{object.inspect}"
-    end
-  end
+      if object[:post]
+        validate_uri(object[:post])
+        post[:post] = object[:post]
+      else
+        raise InvalidResponseError, "Post hash is missing a :post key"
+      end
 
-  def process_post_hash(object)
-    post = {}
+      if object[:reason]
+        post[:reason] = process_post_reason(object[:reason])
+      end
 
-    if object[:post]
-      validate_uri(object[:post])
-      post[:post] = object[:post]
-    else
-      raise InvalidResponseError, "Post hash is missing a :post key"
+      if object[:context]
+        post[:feedContext] = object[:context].to_s
+      end
+
+      post
     end
 
-    if object[:reason]
-      post[:reason] = process_post_reason(object[:reason])
+    def process_post_reason(reason)
+      raise InvalidResponseError, "Invalid post reason: #{reason.inspect}" unless reason.is_a?(Hash)
+
+      if reason[:repost]
+        {
+          "$type" => "app.bsky.feed.defs#skeletonReasonRepost",
+          "repost" => reason[:repost]
+        }
+      elsif reason[:pin]
+        {
+          "$type" => "app.bsky.feed.defs#skeletonReasonPin"
+        }
+      else
+        raise InvalidResponseError, "Invalid post reason: #{reason.inspect}"
+      end
     end
 
-    if object[:context]
-      post[:feedContext] = object[:context].to_s
-    end
-
-    post
-  end
-
-  def process_post_reason(reason)
-    raise InvalidResponseError, "Invalid post reason: #{reason.inspect}" unless reason.is_a?(Hash)
-
-    if reason[:repost]
-      {
-        "$type" => "app.bsky.feed.defs#skeletonReasonRepost",
-        "repost" => reason[:repost]
-      }
-    elsif reason[:pin]
-      {
-        "$type" => "app.bsky.feed.defs#skeletonReasonPin"
-      }
-    else
-      raise InvalidResponseError, "Invalid post reason: #{reason.inspect}"
-    end
-  end
-
-  def validate_uri(uri)
-    if !uri.is_a?(String)
-      raise InvalidResponseError, "Post URI should be a string: #{uri.inspect}"
-    elsif uri !~ AT_URI_REGEXP
-      raise InvalidResponseError, "Invalid post URI: #{uri.inspect}"
+    def validate_uri(uri)
+      if !uri.is_a?(String)
+        raise InvalidResponseError, "Post URI should be a string: #{uri.inspect}"
+      elsif uri !~ AT_URI_REGEXP
+        raise InvalidResponseError, "Invalid post URI: #{uri.inspect}"
+      end
     end
   end
 end
