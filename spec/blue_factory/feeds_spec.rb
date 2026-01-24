7d246ffa24bccc3480a7f262e03eef6b16d9c0f3
Kuba Suder <jakub.suder@gmail.com>
2026-01-24 19:03:53 +0200

    added tests for Feeds

---
+++ spec/blue_factory/feeds_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe BlueFactory do
+  class TestFeed
+    def get_posts(params)
+    end
+  end
+
+  before do
+    BlueFactory.instance_variable_set("@feeds", {})
+  end
+
+  describe '.add_feed' do
+    it 'should register given feed' do
+      feed_one = TestFeed.new
+      feed_two = TestFeed.new
+      feed_three = TestFeed.new
+
+      BlueFactory.add_feed('alpha', feed_one)
+      BlueFactory.add_feed('beta', feed_two)
+      BlueFactory.add_feed('Longest-Allowed', feed_three)
+
+      BlueFactory.feed_keys.should == %w[alpha beta Longest-Allowed]
+      BlueFactory.all_feeds.should == [feed_one, feed_two, feed_three]
+
+      BlueFactory.get_feed('alpha').should == feed_one
+      BlueFactory.get_feed(:beta).should == feed_two
+    end
+
+    it 'should raise an error for invalid keys' do
+      bad_keys = [
+        1,
+        :foo,
+        { blue: true },
+        ['a', 'b'],
+        '',
+        'Llan­fair­pwll­gwyn­gyll­gogery­chwyrn­drob­wlll­lanty­silio­gogo­goch',
+        'blue sky',
+        'gnu/linux',
+        'ohno!'
+      ]
+
+      bad_keys.each do |key|
+        expect { BlueFactory.add_feed(key, TestFeed.new) }.to raise_error(BlueFactory::ConfigurationError)
+      end
+    end
+
+    it "should raise an error when the feed doesn't have a get_posts method" do
+      expect { BlueFactory.add_feed('object', Object.new) }.to raise_error(BlueFactory::InvalidFeedClassError)
+    end
+  end
+end
